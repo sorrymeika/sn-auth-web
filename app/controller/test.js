83397b594b45c58842793fe0d88b334b21acb3a2
孙路 <>
2019-11-17 10:33:43 +0800

    fix test

--- app/controller/test.js
+++ app/controller/test.js
@@ -3,7 +3,7 @@ const { Controller } = require("egg");
 class TestController extends Controller {
     async info() {
         const { ctx } = this;
-        const res = await ctx.service.test.test('asdf', [1, 2]);
+        const res = await ctx.service.test.test({ account: 'admin', password: '12345Qwert', app: 1 });
         ctx.body = {
             name: `hello test`,
             data: res
