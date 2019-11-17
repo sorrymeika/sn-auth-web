b58701dcf87a0e413425f7a455b32909dab1f9d1
孙路 <>
2019-11-17 10:44:39 +0800

    fix test

--- app/controller/test.js
+++ app/controller/test.js
@@ -3,7 +3,7 @@ const { Controller } = require("egg");
 class TestController extends Controller {
     async info() {
         const { ctx } = this;
-        const res = await ctx.service.test.test({ account: 'admin', password: '12345Qwert', app: 1 });
+        const res = await ctx.service.test.test(ctx.request.query);
         ctx.body = {
             name: `hello test`,
             data: res
--- app/service/test.js
+++ app/service/test.js
@@ -1,7 +1,7 @@
 const { Service } = require("egg");
 
 class TestService extends Service {
-    async test(...args) {
+    async test(args) {
         return this.app.authRPC.invoke('auth.login', args);
     }
 }
