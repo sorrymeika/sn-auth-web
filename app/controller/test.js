673f04ebd857ebf09da992c9a7c0dc59bd091ced
孙路 <>
2019-11-17 10:31:48 +0800

    show test res

--- app/controller/test.js
+++ app/controller/test.js
@@ -3,9 +3,10 @@ const { Controller } = require("egg");
 class TestController extends Controller {
     async info() {
         const { ctx } = this;
-        await ctx.service.test.test('asdf', [1, 2]);
+        const res = await ctx.service.test.test('asdf', [1, 2]);
         ctx.body = {
             name: `hello test`,
+            data: res
         };
     }
 }
